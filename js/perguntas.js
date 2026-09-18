aa77a1d49b159c9d9f020f950cf7f474e5da9f4f
kockosamyrah-byte <kocko.samyrah@escola.pr.gov.br>
2026-09-18 16:28:39 -0300

    perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,85 @@
+JavaScript
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está advancing."
+                ],
+                proxima: 1
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                ],
+                proxima: 2
+            }
+        ]
+    },
+    {
+        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo. Como você pretende se posicionar diante do avanço dessa ferramenta?",
+        alternativas: [
+            {
+                texto: "Procurar meios de entender seu funcionamento e aplicar de forma ética.",
+                afirmacao: [
+                    "Criou grupos de ética voltados para IA e busca reduzir desigualdades.",
+                    "Sua preocupação motivou a criar estudos entre trabalhadores para debater o uso ético."
+                ],
+                proxima: 3
+            },
+            {
+                texto: "Evitar o uso e manter preferência por métodos tradicionais.",
+                afirmacao: [
+                    "Achou mais fácil procurar respostas por meios tradicionais mesmo levando mais tempo.",
+                    "Decidiu focar em técnicas manuais e valorizar o trabalho humano."
+                ],
+                proxima: 3
+            }
+        ]
+    },
+    {
+        enunciado: "Que ótimo entusiasmo! De que forma você planeja explorar essa novidade no seu cotidiano?",
+        alternativas: [
+            {
+                texto: "Usando para otimizar tarefas diárias e rotinas de estudo.",
+                afirmacao: [
+                    "Acelerou o processo de criação e organização do seu trabalho.",
+                    "Passou a ensinar outras pessoas a otimizarem suas rotinas."
+                ],
+                proxima: 3
+            },
+            {
+                texto: "Explorando a geração de conteúdos criativos, como artes e textos.",
+                afirmacao: [
+                    "Compartilhou artes nas redes sociais como forma de expressão.",
+                    "Percebeu que a IA empodera pessoas a tirarem ideias do papel."
+                ],
+                proxima: 3
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou que muitas pessoas não sabem utilizar ferramentas tradicionais e compartilhou conhecimento para iniciantes.",
+                    "Estimulou em suas redes sociais a valorização do desenho e pintura tradicionais."
+                ]
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação com geradores de imagem e ensinou quem tinha dificuldade manual.",
+                    "Acredita que a automação na arte é uma forma acessível de comunicação."
+                ]
+            }
+        ]
+    }
+];
